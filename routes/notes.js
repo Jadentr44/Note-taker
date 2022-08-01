9a2f4b499a3c505b1d808a61b12660118739bc84
unknown <jadentr.44@gmail.com>
2022-08-01 16:28:56 -0500

    user can now add notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,22 @@
 const note = require('express').Router();
-const {readFromFile} = require('../helpers/fs')
+const {readFromFile,readAndAppend} = require('../helpers/fs')
 
 note.get('/', (req, res) =>{
   console.log("user get notes");
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+}
+);
+
+note.post('/', (req, res) =>{
+  const {title,text} = req.body;
+  if(req.body){
+
+    const newTask = {title,text}
+    readAndAppend(newTask, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  }else{
+    res.error('error in adding tip')
+  }
 }
 );
 
